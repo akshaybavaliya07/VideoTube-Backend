c827672666b126bf95e217ea8c917b2934757211
Akshay Bavaliya <akshaybavaliya007@gmail.com>
2024-07-16 10:14:36 +0530

    Wrapper function for Asyc function & structure for Response and Error

---
+++ src/utils/ApiError.js
@@ -0,0 +1,17 @@
+class ApiError extends Error {
+    constructor (
+        statusCode,
+        message = "Something went wrong", 
+        errors = [],
+        stack = ""
+    ) {
+        super(message),
+        this.statusCode = statusCode,
+        this.data = null,
+        this.message = message,
+        this.errors = errors
+        stack ? this.stack = stack : Error.captureStackTrace(this, this.constructor)
+    }
+}
+
+export { ApiError }
